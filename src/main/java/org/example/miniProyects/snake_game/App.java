89e20e39981f97b245007389374b1f3e80a1b348
CrisDev <xactrismt1004@gmail.com>
2026-06-08 22:46:14 -0500

    Mini-project: Snake Game

---
+++ src/main/java/org/example/miniProyects/snake_game/App.java
@@ -0,0 +1,22 @@
+package org.example.miniProyects.snake_game;
+
+import javax.swing.*;
+
+public class App {
+    public static void main(String[] args) {
+        int boardWidth = 600;
+        int boardHeight = 600;
+
+        JFrame frame = new JFrame("Snake Game");
+        frame.setVisible(true);
+        frame.setSize(boardWidth, boardHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        var snakeGame = new SnakeGame(boardWidth, boardHeight);
+        frame.add(snakeGame);
+        frame.pack();
+        snakeGame.requestFocus();
+    }
+}
